8fe69822c8e7cc46babfa411aaaf25623176b99c
wyw <wangyw.dev@outlook.com>
2020-04-04 17:37:31 +0800

    update icon

--- build_.bat
+++ build_.bat
@@ -3,5 +3,5 @@ set /p row=<".\main.py"
 set ver=%row:~3,-1%
 echo Building version %ver%
 echo.
-pyinstaller -w -F .\main.py -p .\venv\Lib\site-packages\
+pyinstaller -w -F .\main.py -i icon.ico -p .\venv\Lib\site-packages\
 copy .\dist\main.exe C:\Users\%USERNAME%\Desktop\SysMonitor_%ver%.exe
